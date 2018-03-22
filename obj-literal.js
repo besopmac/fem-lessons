292b61bac96ba12e6c59d4578c3f7431adf7a879
Bruno Campos <bruno@peopleinteractive.com.br>
2018-03-22 14:06:57 -0300

    Object exercises #1

---
+++ obj-literal.js
@@ -0,0 +1,12 @@
+var hero = {
+    name: 'Bruno',
+    age: 35,
+    type: 'Hero',
+    weapon: 'Machine Gun'
+}
+
+var place = [];
+
+place.name  = 'Rio de Janeiro';
+place.level = 'Hard';
+place.population = 1567300;
