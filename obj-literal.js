6f8942a1d92360d562e7e476547d0ca188482bbc
Bruno Campos <bruno@peopleinteractive.com.br>
2018-03-22 19:12:39 -0300

    Object Literal Review

--- obj-literal.js
+++ obj-literal.js
@@ -1,12 +1,31 @@
-var hero = {
-    name: 'Bruno',
-    age: 35,
-    type: 'Hero',
-    weapon: 'Machine Gun'
-}
+/*
+    Create an object using bracket and dot notation that represents
+    the characters and related data you my find in a Game of Clue
+*/
 
-var place = [];
+var game = {};
 
-place.name  = 'Rio de Janeiro';
-place.level = 'Hard';
-place.population = 1567300;
+game.murderer = "?";
+game['weapons'] = [
+    {type: 'laser', location: 'lab'},
+    {type: 'atomic gun' ...},
+    {... 'rock' ...}
+]; 
+game.name = [];
+game.name.push('Miss Scarlet'); // I'd it right on first attempt!
+game.name[0] = ['Mr. Crowley'];
+
+
+
+// var hero = {
+//     name: 'Bruno',
+//     age: 35,
+//     type: 'Hero',
+//     weapon: 'Machine Gun'
+// }
+
+// var place = [];
+
+// place.name  = 'Rio de Janeiro';
+// place.level = 'Hard';
+// place.population = 1567300;
