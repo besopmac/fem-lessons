89009c3d408c2cace835a7c8f553584e20f54811
Bruno Campos <bruno@peopleinteractive.com.br>
2018-03-23 11:56:08 -0300

    Bug fixes

--- obj-literal.js
+++ obj-literal.js
@@ -7,25 +7,18 @@ var game = {};
 
 game.murderer = "?";
 game['weapons'] = [
-    {type: 'laser', location: 'lab'},
-    {type: 'atomic gun' ...},
-    {... 'rock' ...}
+    {   
+        type: 'laser',
+        location: 'lab'
+    },
+    {   type: 'atomic gun',
+        location: 'lab'
+    },
+    {   type: 'rock',
+        location: 'forrest'
+    }
 ]; 
+
 game.name = [];
 game.name.push('Miss Scarlet'); // I'd it right on first attempt!
-game.name[0] = ['Mr. Crowley'];
-
-
-
-// var hero = {
-//     name: 'Bruno',
-//     age: 35,
-//     type: 'Hero',
-//     weapon: 'Machine Gun'
-// }
-
-// var place = [];
-
-// place.name  = 'Rio de Janeiro';
-// place.level = 'Hard';
-// place.population = 1567300;
+game.name[0] = ['Mr. Crowley']; // There is another way to make this...
