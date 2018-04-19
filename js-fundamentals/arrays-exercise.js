aaec5bd4afae65d50764aa081bbf04bd45e8f698
Bruno Campos <brunosopmac@gmail.com>
2018-04-19 00:47:58 -0300

    Nesting noiseArray into animal object

---
+++ js-fundamentals/arrays-exercise.js
@@ -0,0 +1,25 @@
+/* 
+ * create a variable called noiseArray and assign it to
+ * an array literal; place at least one element in the array.
+ */
+
+let noiseArray = [];
+
+/* 
+ * add a noise to the beginning of the noiseArray.
+ * add another noise to the end of the noiseArray.
+ * using bracket notation, add another noise to the end.
+ */
+
+noiseArray.push('buzz');
+noiseArray.push('fizz');
+noiseArray[2] = 'pong';
+
+/* 
+ * 1. what is the length?
+ * 2. what is the last index? How is it different than the length?
+ * 3. inspect your handiwork! What does it look like?
+ */
+
+ console.log(noiseArray.length);
+ console.log(noiseArray.lastIndexOf("pong"));
