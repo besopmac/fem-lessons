c2bc48149d27b2fbcf3434200ac2264931900b7b
Bruno Campos <brunosopmac@gmail.com>
2018-04-29 13:14:34 -0300

    Ending functions section

--- js-fundamentals/farmony.js
+++ js-fundamentals/farmony.js
@@ -40,3 +40,70 @@ let ducks = {
 animals.push(ducks, cats, cartoons);
 
 console.log('Animals length:', animals.length);
+
+
+// eFarmony functions exercises
+// questions 1 & 2
+let AnimalTestUser = function(username) {
+    var argsLenght = arguments.length;
+    var otherArgs = [];
+
+    if (argsLenght > 1) {
+        for (var i=1; i < argsLenght; i++) {
+            otherArgs.push(arguments[i]);
+        }
+    };
+
+    return {
+        'username': username,
+        'otherArgs': otherArgs
+    };
+}
+
+let AnimalTest = function(username) {
+    return {
+        'username': username,
+    };
+}
+
+// questions 3 & 4
+let AnimalCreator = function(username, species, tagline, noises) {
+    return {
+        'username': username,
+        'species': species,
+        'tagline': tagline,
+        'noises': noises,
+        'friends': []
+    }
+}
+
+// question 5
+let addFriend = function(animal, friend) {
+    animal.friends.push(friend.username);
+}
+
+// question 6
+let dog = AnimalCreator('Barney', 'dog', 'through the damn ball, man', ['woof', 'arfarfarf', 'birl']);
+let cow = AnimalCreator('Moo', 'cow', 'got milk?', ['mooo', 'chewchewchew']);
+let horse = AnimalCreator('Sugarfoot', 'horse', '...', ['glump', 'whirrrr']);
+
+let myFarm = [dog, cow, horse];
+addFriend(dog, cow);
+addFriend(cow, horse);
+console.log(myFarm);
+
+// question 7
+let addMatchesArray = function(farm) {
+    for (let animal in farm) {
+        farm[animal].matches = [];
+    }
+}
+addMatchesArray(myFarm);
+
+// question 8
+let giveMatches = function(farm) {
+    for (let animal in farm) {
+        farm[animal].matches.push(farm[animal].friends[0]);
+    }
+}
+giveMatches(myFarm);
