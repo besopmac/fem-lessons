0c7c3f2c768fdc43dbcb894af89125188ac84ab1
Bruno Campos <brunosopmac@gmail.com>
2018-08-06 16:11:15 -0300

    Challenge AJAX

--- web-dev-into/doggos.js
+++ web-dev-into/doggos.js
@@ -1,5 +1,4 @@
 const dog_url = 'https://dog.ceo/api/breeds/image/random';
-
 const doggos = document.querySelector('.doggos');
 
 function addNewDoggo() {
@@ -8,15 +7,25 @@ function addNewDoggo() {
     promise
         .then(function(response) {
             const processingPromise = response.json();
+            
+            const img = document.createElement('img');
+            img.alt = 'Loading...';
+            img.src = 'load.gif';
+            
+            doggos.appendChild(img);
+
+            
             return processingPromise;
         })
         .then(function(processedResponse) {
-            const img = document.createElement('img');
+            img = document.querySelector('img');
             img.src = processedResponse.message;
             img.alt = 'Cute doggo';
+
             doggos.appendChild(img);
+
+            console.log(img);
         });
 }
-
 
 document.querySelector('.add-doggo').addEventListener('click', addNewDoggo);
